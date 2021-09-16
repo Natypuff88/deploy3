f34cbb2e1983151854ac3281195b59de644a771f
Natypuff88 <70305401+Natypuff88@users.noreply.github.com>
2021-09-15 22:14:51 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,9 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-# si los cambios se suben a https://<USERNAME>.github.io/ ejecutar lo
-siguiente
-git push -f git@github.com:Natypuff88/deploytres.github.io.git master
-read -p "Cambios actualizados en Bitbucket. Presione [Enter] para
-continuar..."
-# si los cambios se suben a https://<USERNAME>.github.io/<REPO> ejecutar
-lo siguiente
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# si los cambios se suben a https://<USERNAME>.github.io/ ejecutar lo siguiente
+
+git push -f https://github.com/Natypuff88/deploytres.git master:gh-pages 
+
 cd -
